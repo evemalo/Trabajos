936ad219926aece087a7386dbf16e5db1454b376
evemalo <evamalo734@gmail.com>
2023-03-29 09:11:19 -0500

    v5

---
+++ ejer.py
@@ -0,0 +1,13 @@
+nombre="Evelyn"
+apellido="Malo"
+print(nombre , apellido)
+print(nombre*5)
+print(len(nombre))
+numero=int(input("Ingrese el numero: "))
+print(type(numero))
+cadena=str(numero)
+print(type(cadena))
+y=8.17
+type(y)
+p=12.34
+type(p)
