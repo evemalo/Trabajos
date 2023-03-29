ac9d82e40498f04a045d3e4f9a1e708a07f2eebb
evemalo <evamalo734@gmail.com>
2023-03-29 08:08:38 -0500

    v5

---
+++ entrada de datos.py
@@ -0,0 +1,11 @@
+num1=int(input("Ingrese num1: "))
+num2=int(input("Ingrese num2: "))
+x=num1 == num2
+a=num1 != num2
+b=num1 > num2
+c=num1 <= num2
+print("Es igual ",x)
+print("Son diferentes ",a)
+print("El primer número es mayor ",b)
+print("El segundo número es mayor o igual ",c)
+
